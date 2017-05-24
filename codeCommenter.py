92c8af1d281585487adb1d333cc9f565685438f1
Sampo <sampo.siltanen@gmail.com>
2017-05-25 00:06:44 +0300

    Added logic to create backup versions of files being updated. Also updated README

--- codeCommenter.py
+++ codeCommenter.py
@@ -1,22 +1,32 @@
-##################################################################################
+#############################################################################################
 #
-# Sampo Siltanen 
+# Sampo Siltanen
 # 2017
-# 
-# This script crawls through C++ source files within given directory and produces 
-# comment templates for functions in .h file based on the corresponding .cpp file.
-# The comment template are formatted so that doxygen can read them
 #
-# Use of this software is at the user's own risk. 
+# This script crawls through C++ source files within given directory and produces
+# comment templates for functions in .h file based on the corresponding .cpp file.
+# The comment template are formatted so that doxygen can read them.
+#
+# Software asks for two paths:
+#   1. Path to where it creates the backup files
+#   2. Path from where it searches for the files
+#
+# User may give these in this order as command line parameters or during run-time.
+# If no parameters are provided or they are not valid, the user is asked for new paths.
+#
+# The software does not detect file name collisions when making backups but instead
+# renames the files with seconds and microseconds from timestamp to make sure they are unique.
+#
+# Use of this software is at the user's own risk.
 # I do not take any responsibility for mishaps caused by the use of this software.
 #
-#################################################################################
-
-
+#############################################################################################
 
 import os
+import datetime
 import re
 import sys
+from shutil import copy2
 
 #Class to represent functions in cpp code
 class Function(object):
@@ -37,6 +47,51 @@ class ProcessedFunction(object):
         self.postconditions = postconds[:]
 
 
+#Main function for this script
+def main():
+    #Process path to backup files
+    bupath = ''
+    if (len(sys.argv) > 2):
+        print('Entered path for backup files: ' + sys.argv[1])
+        bupath = sys.argv[1]
+    while True:
+        if (len(bupath) == 0):
+            bupath = input("Enter path to backup file folder: ")
+        try:
+            os.chdir(bupath)
+            break
+        except (WindowsError, OSError):
+            print('Oops, no such folder found')
+            bupath = ''
+
+    #Create name for backup directory
+    bupath = os.path.join(os.getcwd(), 'CodeCommenter_bu_' + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '\\')
+
+    #Process path from where files are loaded
+    path = ''
+    if (len(sys.argv) > 2):
+        print('Entered path: ' + sys.argv[2])
+        path = sys.argv[2]
+    while True:
+        if (len(path) == 0):
+            path = input("Enter path to folder: ")
+        try:
+            os.chdir(path)
+            break
+        except (WindowsError, OSError):
+            print('Oops, no such folder found')
+            path = ''
+
+    #Process contents of working directory
+    headerFiles = searchFilesRecursively()
+    updatedFilesCount = 0
+    for file in headerFiles:
+        if (processHeaderFile(file, bupath)):
+            updatedFilesCount += 1
+    print('Total of ', updatedFilesCount, ' files were updated')
+
+
+
 #Fetches paths to all header files in current working directory and directories in it
 def searchFilesRecursively():
     contents = os.listdir()
@@ -48,7 +103,7 @@ def searchFilesRecursively():
             headers.append('{}\{}'.format(os.getcwd(), item))
         elif ('.' not in item):
             directories.append(item)
-            
+
     #Process folders recursively
     for directory in directories:
         try:
@@ -56,8 +111,8 @@ def searchFilesRecursively():
             headers += searchFilesRecursively()
             os.chdir('..')
         except (WindowsError, OSError):
-            print('Oops, error in recursive folder processing')
-    
+            print('Oops, error in recursive folder processing. ' + directory + ' was not a folder')
+
     return headers
 
 
@@ -72,19 +127,19 @@ def crawlFunctionDefinitions(filename):
     cpp = open(filename[:-1] + 'cpp', "r")
     cppfile = cpp.readlines()
     cpp.close()
-    
+
     #List to hold Function objects
     functions = list()
     preconditions = list()
     postconditions = list()
-    
+
     #Variables to keep track of function begin and end, and comment blocks
     openBrackets = 0
     closedBrackets = 0
     openParentheses = 0
     closedParentheses = 0
     commentsBlock = False
-    
+
     #Loop through cpp file
     prevLineTemp = ""
     for line in cppfile:
@@ -102,15 +157,15 @@ def crawlFunctionDefinitions(filename):
         elif ('*/' in line):
             line = line[line.index('*/') + 2:]
             commentsBlock = False
- 
+
         #Skip line if it is empty after cleaning the comments from it
         if (len(line.strip()) == 0):
             continue
-        
+
         #Skip includes and declares
         if (line.lstrip()[:8] == '#include' or line.lstrip()[:8] == '#declare'):
             continue
-        
+
         if (openBrackets == closedBrackets):
             #Manage functions with open bracket at the end of the line
             if ('{' in line):
@@ -118,7 +173,7 @@ def crawlFunctionDefinitions(filename):
                 closedBrackets += line.count('}')
                 #Leave implementation out
                 line = line[:line.index('{')]
-            
+
             openParentheses += line.count('(')
             closedParentheses += line.count(')')
             prevLineTemp += line.lstrip(' \t').rstrip('\n')
@@ -134,7 +189,7 @@ def crawlFunctionDefinitions(filename):
                 del preconditions[:]
                 del postconditions[:]
             continue
-        
+
         #Line does not hold function title
         else:
             if ('REQUIRE' in line):
@@ -180,7 +235,7 @@ def processFunctions(functions):
             returnValue = d[:d.index(' ')]
             funcName = d[d.index(' ') + 1:d.index('(')]
             classlessDef = d
-        
+
         #Check if there are any parameters
         parameterNames = list()
         if (len(d[d.index('(') + 1:d.rindex(')')].strip(' ')) > 0):
@@ -189,7 +244,7 @@ def processFunctions(functions):
             parameters = [p.rstrip(' ') for p in d[d.index('(') + 1:d.rindex(')')].split(',')]
             #collect names of parameters
             parameterNames = [p[p.rindex(' ') + 1:] for p in parameters]
-        
+
         processedFuncs.append(ProcessedFunction(classlessDef, returnValue, className, funcName, parameterNames, func.pres, func.posts))
 
     return processedFuncs
@@ -209,23 +264,23 @@ def getLineFromListBySubstring(lst, substr):
 #   There isn't comments already for the function in header file
 #Returns true if file was updated
 #Returns false if file was not updated
-def processHeaderFile(filename):
+def processHeaderFile(filename, backupPath):
     functions = crawlFunctionDefinitions(filename)
     if (len(functions) > 0):
         processedFuncs = processFunctions(functions)
-        
+
         h = open(filename, "r")
-        hfile = h.readlines() 
+        hfile = h.readlines()
         h.close()
-        upd_hfile = list()        
-        
+        upd_hfile = list()
+
         commentsBlock = False
         existingComments = list()
         newLines = list()
         multilineFuncDef = list()
         isMultilineFunc = False
         for line in hfile:
-            
+
             #Handle functions that are split on several lines
             if (line.count('(') > line.count(')')):
                 multilineFuncDef.append(line.rstrip())
@@ -237,13 +292,13 @@ def processHeaderFile(filename):
             if (isMultilineFunc):
                 multilineFuncDef.append(line.rstrip())
                 continue
-            
+
             #Join the multiline function to single line
             if(len(multilineFuncDef) > 0):
                 line = ""
                 for l in multilineFuncDef:
                     line += l
-            
+
             #Create line without whitespaces in order to compare data in lines reliably
             strippedLine = "".join(line.split())
 
@@ -257,7 +312,7 @@ def processHeaderFile(filename):
             if (commentsBlock):
                 existingComments.append(line)
                 continue
-            
+
             if (';' in line):
                 for f in processedFuncs:
                     #Match function definitions without spaces and tabs
@@ -266,72 +321,79 @@ def processHeaderFile(filename):
                         #Builder is an expection because it passes the previous condition when compared to destructor
                         if (f.name == f.className and '~' in strippedLine):
                             continue
-                        
+
                         #Set formatting of the list
                         if(len(upd_hfile[-1].strip()) > 0):
                             newLines.append('')
                         indent = '\t ' if re.match(r'\s', line) else ' '
                         newLines.append(indent[:-1] + '/**')
-                        
+
                         #Update existing comments if there were any
                         if(len(existingComments) > 0):
                             commentLine = getLineFromListBySubstring(existingComments, '* \\brief')
                             newLines.append(indent + (commentLine.strip() if len(commentLine) > 0 else '* \\brief ' + f.name))
-                            
+
                             for p in f.parameterNames:
                                 commentLine = getLineFromListBySubstring(existingComments, '* \\param ' + p)
                                 newLines.append(indent + (commentLine.strip() if len(commentLine) > 0 else '* \\param ' + p))
-                                
+
                             for pre in f.preconditions:
                                 commentLine = getLineFromListBySubstring(existingComments, '* \\pre ' + pre)
                                 newLines.append(indent + (commentLine.strip() if len(commentLine) > 0 else '* \\pre ' + pre))
-                                
+
                             for post in f.postconditions:
                                 commentLine = getLineFromListBySubstring(existingComments, '* \\post ' + post)
                                 newLines.append(indent + (commentLine.strip() if len(commentLine) > 0 else '* \\post ' + post))
-                            
+
                             if(f.returnValue != 'void' and f.returnValue != ''):
                                 commentLine = getLineFromListBySubstring(existingComments, '* \\return')
                                 newLines.append(indent + (commentLine.strip() if len(commentLine) > 0 else '* \\return '))
-                            
+
                             del existingComments[:]
                         #No existing comments, create new ones
                         else:
                             newLines.append(indent + '* \\brief ' + f.name)
-                            
+
                             for p in f.parameterNames:
                                 newLines.append(indent + '* \\param ' + p)
-                                
+
                             for pre in f.preconditions:
                                 newLines.append(indent + '* \\pre ' + pre)
-                                
+
                             for post in f.postconditions:
                                 newLines.append(indent + '* \\post ' + post)
-                                
+
                             if (f.returnValue != 'void' and f.returnValue != ''):
                                 newLines.append(indent + '* \\return ')
-                        
+
                         newLines.append(indent + '*/')
                         processedFuncs.remove(f)
-            
-            #If new comment lines were formed, append them before the current line                
+
+            #If new comment lines were formed, append them before the current line
             if (len(newLines) > 0):
                 upd_hfile.extend(newLines)
                 del newLines[:]
-            
+
             #If function was split on multiple lines, append the multiline version
             if (len(multilineFuncDef) > 0):
                 upd_hfile.extend(multilineFuncDef)
                 del multilineFuncDef[:]
             else:
                 upd_hfile.append(line.rstrip())
-        
+
+        #Create backup directory only when it is actually needed
+        if not os.path.exists(backupPath):
+            os.makedirs(backupPath)
+
+        #Create backup version of the file being updated
+        copy2(filename, backupPath + '\\' + filename[filename.rfind('\\') + 1:] + '.bak_' + datetime.datetime.now().strftime('%S%f'))
+
         #Write the processed lines back to file
         newFile = open(filename, "w")
         for row in upd_hfile:
             newFile.write("%s\n" % row)
         newFile.close()
-        
+
         return True
     else:
         return False
@@ -341,30 +403,8 @@ def processHeaderFile(filename):
 
 ###########################################
 #
-#Script entry point
+# Script entry point
 #
 ###########################################
 if (__name__ == "__main__"):
-	path = ''
-	if (len(sys.argv) > 1):
-		print('Entered path: ' + sys.argv[1])
-		path = sys.argv[1]
-	while True:
-		if (len(path) == 0):
-			path = input("Enter path to folder: ")
-		else:
-			print(path)
-		try:
-			os.chdir(path)
-			break
-		except (WindowsError, OSError):
-			print('Oops, no such folder found')
-			path = ''
-
-	#Process contents of working directory
-	headerFiles = searchFilesRecursively()
-	updatedFilesCount = 0
-	for file in headerFiles:
-		if (processHeaderFile(file)):
-			updatedFilesCount += 1
-	print('Total of ', updatedFilesCount, ' files were updated')
+    main()
