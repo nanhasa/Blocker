faeb6e6d2027f179929ea04e0395de18fa01be5c
Sampo <sampo.siltanen@gmail.com>
2017-08-23 16:27:38 +0300

    Added IEvent interface

--- Source/interfaces.h
+++ Source/interfaces.h
@@ -6,26 +6,35 @@
 
 #pragma warning (push, 2)  // Temporarily set warning level 2
 #include <3rdParty/glm/glm.hpp>
-#include <3rdParty/glm/gtc/matrix_transform.hpp>
-#include <3rdParty/glm/gtc/type_ptr.hpp>
 #pragma warning (pop)      // Restore back
 
 class IRenderer {
 public:
 	virtual ~IRenderer() {};
 
-	virtual bool initialize(std::string&& windowName, int width, int height, 
+	virtual bool vInitialize(std::string&& windowName, int width, int height, 
 		std::function<void(float)>&& gameLogic) = 0;
-	virtual void startMainLoop() = 0;
-	virtual glm::vec3 getCameraFront() = 0;
+	virtual void vStartMainLoop() = 0;
+	virtual glm::vec3 vGetCameraFront() = 0;
 };
 
 class IImageType {
 public:
 	virtual ~IImageType() {};
 
-	virtual bool loadFile(std::ifstream& stream) = 0;
-	virtual std::unique_ptr<uint8_t[]> decode() = 0;
-	virtual int getHeight() const = 0;
-	virtual int getWidth() const = 0;
+	virtual bool vLoadFile(std::ifstream& stream) = 0;
+	virtual std::unique_ptr<uint8_t[]> vDecode() = 0;
+	virtual int vGetHeight() const = 0;
+	virtual int vGetWidth() const = 0;
+};
+
+using eventType = uint32_t;
+
+class IEvent {
+public: 
+	virtual ~IEvent() {};
+
+	virtual eventType vGetEventType() const = 0;
+	virtual float vGetCreateTime() const = 0;
+	virtual std::string vGetEventName() const = 0;
 };
