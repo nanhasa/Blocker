a587b02a2c352b44ee1a1eee79595e42126037b9
Sampo <sampo.siltanen@gmail.com>
2017-08-27 20:36:14 +0300

    Created new test project using google test framework

---
+++ Test/BlockerTest/BlockerTest.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"  
+#include <iostream>
+
+#include "3rdParty/gtest/gtest.h"
+
+int main(int argc, char** argv) {
+	testing::InitGoogleTest(&argc, argv);
+	RUN_ALL_TESTS();
+	std::getchar(); // keep console window open until Return keystroke
+}
