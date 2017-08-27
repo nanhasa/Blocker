c9237b096566efe3196f24e1b078b9e782b9b530
Sampo <sampo.siltanen@gmail.com>
2017-08-27 23:02:08 +0300

    Fixed problems in paths

--- Scripts/BatchFiles/blocker_post_build_code_commenting.bat
+++ Scripts/BatchFiles/blocker_post_build_code_commenting.bat
@@ -1,7 +1,7 @@
 @echo off
 where python.exe >nul 2>nul
 if %errorlevel% EQU 0 (
-	if exist %~dp0..\..\Scripts\CodeCommenter\codeCommenter.py (
-		python %~dp0..\..\Scripts\CodeCommenter\codeCommenter.py %~dp0..\..\Scripts\CodeCommenter\ %~dp0..\
+	if exist %~dp0..\CodeCommenter\codeCommenter.py (
+		python %~dp0..\CodeCommenter\codeCommenter.py %~dp0..\CodeCommenter\ %~dp0..\..\Source\
 	)
 )
