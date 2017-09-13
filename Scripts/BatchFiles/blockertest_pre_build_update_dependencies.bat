f72bc11cf2e58dd647efc026795dd86e209b46ba
Sampo <sampo.siltanen@gmail.com>
2017-09-13 20:12:00 +0300

    Removed extra slashes from paths

--- Scripts/BatchFiles/blockertest_pre_build_update_dependencies.bat
+++ Scripts/BatchFiles/blockertest_pre_build_update_dependencies.bat
@@ -5,6 +5,6 @@ REM then runs the file from solution file perspective
 where python.exe >nul 2>nul
 if %errorlevel% EQU 0 (
 	if exist %~dp0..\PyCppUtility\ObjDependencyUpdater\dependencyUpdater.py (
-		python %~dp0..\PyCppUtility\ObjDependencyUpdater\dependencyUpdater.py BlockerTest %~dp0\..\..\Test\BlockerTest %~dp0\..\..\Temp\BlockerWin32Debug %~dp0\..\..\Temp\BlockerWin32Release
+		python %~dp0..\PyCppUtility\ObjDependencyUpdater\dependencyUpdater.py BlockerTest %~dp0..\..\Test\BlockerTest %~dp0..\..\Temp\BlockerWin32Debug %~dp0..\..\Temp\BlockerWin32Release
 	)
 )
