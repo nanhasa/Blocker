ffd938249b6b06bdb7ec7dc22ba3dad3a11db34c
Sampo <sampo.siltanen@gmail.com>
2017-09-29 02:01:47 +0300

    Changed timestamp to be milliseconds from game start, renamed old function as timeSinceEpoch. Added lerp function to be used for now

--- Source/Utility/utility.cpp
+++ Source/Utility/utility.cpp
@@ -1,8 +1,10 @@
-#include <chrono>
-
 #include "Utility/utility.h"
 
-long utility::timestampMs()
+#include <chrono>
+
+static const auto gameStartTime = std::chrono::system_clock::now();
+
+long utility::timeSinceEpoch()
 {
 	using namespace std::chrono;
 	const auto now = time_point_cast<milliseconds>(system_clock::now());
@@ -10,12 +12,32 @@ long utility::timestampMs()
 	return static_cast<long>(now.time_since_epoch().count());
 }
 
-float utility::deltaTimeSec(long timestamp)
+int utility::timestampMs()
+{
+	using namespace std::chrono;
+	auto now = system_clock::now();
+	return static_cast<int>(duration_cast<milliseconds>(now - gameStartTime).count());
+}
+
+float utility::deltaTimeSec(int timestamp)
 {
 	return static_cast<float>((timestampMs() - timestamp) / 1000);
 }
 
-int utility::deltaTimeMs(long timestamp)
+int utility::deltaTimeMs(int timestamp)
 {
 	return static_cast<int>(timestampMs() - timestamp);
+}
+
+glm::vec3 utility::lerp(const glm::vec3 & start, const glm::vec3 & end, float percentage)
+{
+	// Clamp percentage between 0..1
+	if (percentage > 1.0f) percentage = 1.0f;
+	if (percentage < 0.0f) percentage = 0.0f;
+
+	// Start to end vector
+	auto startToEnd = end - start;
+
+	// Base vector to start and add percentage of startToEnd vector
+	return start + startToEnd * percentage;
 }
--- Source/Utility/utility.h
+++ Source/Utility/utility.h
@@ -1,24 +1,43 @@
 #pragma once
 
+#pragma warning (push, 2)  // Temporarily set warning level 2
+#include <3rdParty/glm/glm.hpp>
+#pragma warning (pop)      // Restore back
+
 namespace utility {
+
+	/**
+	 * \brief timeSinceEpoch
+	 * \return 
+	 */
+	long timeSinceEpoch();
 
 	/**
 	 * \brief Used to get timestamp in milliseconds, thread safe
 	 * \return Current time in milliseconds
 	 */
-	long timestampMs();
+	int timestampMs();
 
 	/**
 	 * \brief deltaTimeSec
 	 * \param timestamp
 	 * \return Elapsed seconds since the parameter value
 	 */
-	float deltaTimeSec(long timestamp);
+	float deltaTimeSec(int timestamp);
 
 	/**
 	 * \brief deltaTimeMs
 	 * \param timestamp
 	 * \return Elapsed milliseconds since the parameter value
 	 */
-	int deltaTimeMs(long timestamp);
+	int deltaTimeMs(int timestamp);
+
+	/**
+	 * \brief lerp
+	 * \param start
+	 * \param end
+	 * \param percentage
+	 * \return 
+	 */
+	glm::vec3 lerp(const glm::vec3& start, const glm::vec3& end, float percentage);
 }
