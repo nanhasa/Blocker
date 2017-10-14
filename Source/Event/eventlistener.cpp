deae63aed60be3d648926260becd1cc9424dd5e9
Sampo <sampo.siltanen@gmail.com>
2017-10-14 12:40:05 +0300

    Changed eventmanager calls to go through service locator

--- Source/Event/eventlistener.cpp
+++ Source/Event/eventlistener.cpp
@@ -3,17 +3,18 @@
 #include <algorithm>
 
 #include "Event/eventManager.h"
+#include "Utility/locator.h"
 
-EventListener::EventListener() : m_listenerId(EventManager::registerListener()) {}
+EventListener::EventListener() : m_listenerId(Locator::getEventManager()->registerListener()) {}
 
 EventListener::~EventListener()
 {
-	for (auto evt : m_eventsListened) { EventManager::removeListener(evt, m_listenerId); }
+	for (auto evt : m_eventsListened) { Locator::getEventManager()->removeListener(evt, m_listenerId); }
 }
 
 bool EventListener::registerForEvent(const EventType evtType, std::function<void(std::shared_ptr<IEvent>)> evtDelegate)
 {
-	if (EventManager::addListener(evtType, m_listenerId, std::move(evtDelegate))) {
+	if (Locator::getEventManager()->addListener(evtType, m_listenerId, std::move(evtDelegate))) {
 		m_eventsListened.emplace_back(evtType);
 		return true;
 	}
@@ -22,7 +23,7 @@ bool EventListener::registerForEvent(const EventType evtType, std::function<void
 
 bool EventListener::unregisterForEvent(const EventType evtType)
 {
-	if (EventManager::removeListener(evtType, m_listenerId)) {
+	if (Locator::getEventManager()->removeListener(evtType, m_listenerId)) {
 		std::remove(m_eventsListened.begin(), m_eventsListened.end(), evtType);
 		return true;
 	}
