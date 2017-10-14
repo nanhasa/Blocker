2b37526b59377859c41e950186b80abde1c54705
Sampo <sampo.siltanen@gmail.com>
2017-10-14 12:45:49 +0300

    Added Service Locator provide object calls

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,9 +1,13 @@
-#include <iostream>
-
+#include "Event/eventmanager.h"
 #include "GameManager/gamemanager.h"
+#include "Utility/config.h"
+#include "Utility/locator.h"
 
 int main()
 {
+	Locator::provideConfig(std::make_unique<Config>());
+	Locator::provideEventManager(std::make_unique<EventManager>());
+
 	GameManager gm;
 	if (!gm.start())
 		return EXIT_FAILURE;
