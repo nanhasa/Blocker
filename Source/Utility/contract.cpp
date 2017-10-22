6980ff44b336d3cd956f12f6461be30a567c42d1
Sampo <sampo.siltanen@gmail.com>
2017-10-22 16:19:38 +0300

    Changed Logger to be StaticSafeLogger

--- Source/Utility/contract.cpp
+++ Source/Utility/contract.cpp
@@ -3,12 +3,12 @@
 #include <cstdio>
 #include <cstdlib>
 
-#include "Utility/logger.h"
+#include "Utility/staticsafelogger.h"
 #include "utility.h"
 
 void abortProgram(const char* file, const int line, const char* condition, const char* type)
 {
-	static Logger log("Contract");
+	static StaticSafeLogger log("Contract");
 	log.fatal(std::string(type) + " condition " + std::string(condition) + 
 		" failed on line " + utility::toStr(line) + " in file " + std::string(file));
 	exit(EXIT_FAILURE);
